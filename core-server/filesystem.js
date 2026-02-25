bb883e0caff3abed6c0ca9fa0e8366b85696989b
linonetwo <linonetwo012@gmail.com>
2026-02-26 04:02:40 +0800

    feat(markdown): support frontmatter metadata and save-time meta migration

--- core-server/filesystem.js
+++ core-server/filesystem.js
@@ -267,6 +267,9 @@ exports.generateTiddlerFileInfo = function(tiddler,options) {
 				}
 			}
 		}
+		if(fileInfo.hasMetaFile && fileInfo.type !== "application/x-tiddler" && fileInfo.type !== "application/json" && $tw.Wiki.tiddlerSerializerModules && $tw.Wiki.tiddlerSerializerModules[fileInfo.type]) {
+			fileInfo.hasMetaFile = false;
+		}
 	}
 	// Take the file extension from the tiddler content type or metaExt
 	var contentTypeInfo = $tw.config.contentTypeInfo[fileInfo.type] || {extension: ""};
@@ -416,6 +419,7 @@ Save a tiddler to a file described by the fileInfo:
 */
 exports.saveTiddlerToFile = function(tiddler,fileInfo,callback) {
 	$tw.utils.createDirectory(path.dirname(fileInfo.filepath));
+	var serializer = $tw.Wiki.tiddlerSerializerModules && $tw.Wiki.tiddlerSerializerModules[fileInfo.type];
 	if(fileInfo.hasMetaFile) {
 		// Save the tiddler as a separate body and meta file
 		var typeInfo = $tw.config.contentTypeInfo[tiddler.fields.type || "text/plain"] || {encoding: "utf8"};
@@ -434,6 +438,19 @@ exports.saveTiddlerToFile = function(tiddler,fileInfo,callback) {
 		// Save the tiddler as a self contained templated file
 		if(fileInfo.type === "application/x-tiddler") {
 			fs.writeFile(fileInfo.filepath,tiddler.getFieldStringBlock({exclude: ["text","bag"]}) + (!!tiddler.fields.text ? "\n\n" + tiddler.fields.text : ""),"utf8",function(err) {
+				if(err) {
+					return callback(err);
+				}
+				return callback(null,fileInfo);
+			});
+		} else if(serializer) {
+			var serialized;
+			try {
+				serialized = serializer(tiddler);
+			} catch(err) {
+				return callback(err);
+			}
+			fs.writeFile(fileInfo.filepath,serialized,"utf8",function(err) {
 				if(err) {
 					return callback(err);
 				}
@@ -458,6 +475,7 @@ Save a tiddler to a file described by the fileInfo:
 */
 exports.saveTiddlerToFileSync = function(tiddler,fileInfo) {
 	$tw.utils.createDirectory(path.dirname(fileInfo.filepath));
+	var serializer = $tw.Wiki.tiddlerSerializerModules && $tw.Wiki.tiddlerSerializerModules[fileInfo.type];
 	if(fileInfo.hasMetaFile) {
 		// Save the tiddler as a separate body and meta file
 		var typeInfo = $tw.config.contentTypeInfo[tiddler.fields.type || "text/plain"] || {encoding: "utf8"};
@@ -467,6 +485,8 @@ exports.saveTiddlerToFileSync = function(tiddler,fileInfo) {
 		// Save the tiddler as a self contained templated file
 		if(fileInfo.type === "application/x-tiddler") {
 			fs.writeFileSync(fileInfo.filepath,tiddler.getFieldStringBlock({exclude: ["text","bag"]}) + (!!tiddler.fields.text ? "\n\n" + tiddler.fields.text : ""),"utf8");
+		} else if(serializer) {
+			fs.writeFileSync(fileInfo.filepath,serializer(tiddler),"utf8");
 		} else {
 			fs.writeFileSync(fileInfo.filepath,JSON.stringify([tiddler.getFieldStrings({exclude: ["bag"]})],null,$tw.config.preferences.jsonSpaces),"utf8");
 		}
@@ -535,6 +555,22 @@ exports.cleanupTiddlerFiles = function(options,callback) {
 			}
 			return callback(null,bootInfo);
 		});
+	} else if(adaptorInfo.filepath && bootInfo.filepath && adaptorInfo.filepath === bootInfo.filepath && adaptorInfo.hasMetaFile && !bootInfo.hasMetaFile) {
+		var oldMetaPath = adaptorInfo.filepath + ".meta";
+		if(fs.existsSync(oldMetaPath)) {
+			fs.unlink(oldMetaPath,function(err) {
+				if(err) {
+					if((err.code == "EPERM" || err.code == "EACCES") && err.syscall == "unlink") {
+						$tw.syncer.displayError("Server desynchronized. Error cleaning up previous metafile for tiddler: \"" + title + "\"",err);
+						return callback(null,bootInfo);
+					}
+					return callback(err);
+				}
+				return callback(null,bootInfo);
+			});
+		} else {
+			return callback(null,bootInfo);
+		}
 	} else {
 		return callback(null,bootInfo);
 	}
